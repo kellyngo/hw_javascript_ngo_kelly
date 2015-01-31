c9e9761159a7150e6f7eb4821e2354fb4064db04
Kelly Ngo <kellyngo726@gmail.com>
2015-01-31 18:06:52 -0500

    functions

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function howdy() {
+    console.log("Kelly Ngo says howdy!")
+}
+
+howdy();
+
+var numba = function string () {
+    if (string.length < 7) {
+        console.log("What a short little word!")
+    }
+    else (string.length > 7) {
+        console.log("I'm sorry, but that's too many words to count")
+    }
+    else (string.length == 7) {
+        console.log("Seven, what a great choice!")
+    }
+}
+
+string(7);
+
+string("seventy");
+
+string("university");
+
+
+function inception ([callback], [value]) {
+    if (callback.length > 1) {
+        console.log("Inception is a fantastic movie!")
+}
+
+inception [callback] ("inception");
